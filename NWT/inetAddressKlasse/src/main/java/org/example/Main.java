efb481cfbbfed4189ac6a52b61e53301533c4715
Amro.Rehawi <amro.rehawi@lbs4.salzburg.at>
2023-04-18 11:20:16 +0200

    mylookup Program

---
+++ NWT/inetAddressKlasse/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+public class Main {
+    public static void main(String[] args) throws UnknownHostException {
+
+        nwt.mylookup("gsagle.com");
+        nwt.mylookup("example.com");
+        nwt.mylookup("8.8.8.8");
+
+    }
+}
